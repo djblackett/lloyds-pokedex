364614e468ff7c4cc442e5ab1f7671f31e6f8149
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-10-30 20:52:40 +0000

    Created helper function

--- client/src/utils/helpers/index.ts
+++ client/src/utils/helpers/index.ts
@@ -26,3 +26,23 @@ export const fetchAndMapPokemonList = async (): Promise<MappedPokemon[]> => {
   const data: PokemonListData = await response.json();
   return mapPokemonList(data);
 };
+
+export const findPokemonId = (pokemonList: MappedPokemon[], name: string) => {
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === name);
+  return pokemon ? pokemon.id : null;
+};
+
+export const findPreviousAndNextPokemon = (
+  pokemonList: MappedPokemon[],
+  name: string
+) => {
+  const pokemonId = findPokemonId(pokemonList, name);
+  if (pokemonId === null) {
+    return { previous: null, next: null };
+  }
+
+  const previous = pokemonList?.find((pokemon) => pokemon.id === pokemonId - 1);
+  const next = pokemonList?.find((pokemon) => pokemon.id === pokemonId + 1);
+
+  return { previous, next };
+};
