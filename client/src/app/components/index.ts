b1ac67c8c9d16508f7d32d9fb405a6f40c143513
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-10-30 13:04:16 +0000

    default exports

---
+++ client/src/app/components/index.ts
@@ -0,0 +1,3 @@
+export { default as PokemonList } from "./PokemonList/PokemonList";
+export { default as PokemonAbility } from "./PokemonAbility/PokemonAbility";
+export { default as PokemonPage } from "./PokemonPage/PokemonPage";
