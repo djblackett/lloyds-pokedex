8d0966848869b8657c65e210e6eff8229ab8fefb
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-10-30 20:52:02 +0000

    converted to .tsx

---
+++ client/src/app/pokemon/[name]/page.tsx
@@ -0,0 +1,21 @@
+import { PokemonPage } from "@/app/components";
+import {
+  fetchAndMapPokemonList,
+  findPreviousAndNextPokemon,
+} from "@/utils/helpers";
+
+interface Params {
+  params: {
+    name: string;
+  };
+}
+
+const page = async ({ params }: Params) => {
+  const { name } = params;
+  const pokemonList = await fetchAndMapPokemonList();
+  const { previous, next } = findPreviousAndNextPokemon(pokemonList, name);
+
+  return <PokemonPage name={name} previous={previous} next={next} />;
+};
+
+export default page;
