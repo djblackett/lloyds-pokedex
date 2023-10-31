c7389015e1bbc6fc842a4b5c12815c3fcf98ec14
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-10-31 06:39:35 +0000

    Created helper

--- client/src/utils/helpers/index.ts
+++ client/src/utils/helpers/index.ts
@@ -46,3 +46,9 @@ export const findPreviousAndNextPokemon = (
 
   return { previous, next };
 };
+
+export const fetchPokemon = async (name: string) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const data = await response.json();
+  return data;
+};
