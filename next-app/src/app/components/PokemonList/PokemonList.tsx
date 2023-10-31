1df5a1fea5e4f9eef8c88de697ef8f807e8a5aa0
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-10-31 21:28:53 +0000

    remove css and add more tailwind

--- next-app/src/app/components/PokemonList/PokemonList.tsx
+++ next-app/src/app/components/PokemonList/PokemonList.tsx
@@ -9,7 +9,7 @@ const PokemonList = ({ pokemonList }: { pokemonList: MappedPokemon[] }) => {
           <Link
             key={id}
             href={`/pokemon/${name}`}
-            className="list-item"
+            className="bg-[#eee] bg-center bg-no-repeat m-2 h-36 w-36 relative"
             style={{
               // eslint-disable-next-line max-len
               backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})`,
