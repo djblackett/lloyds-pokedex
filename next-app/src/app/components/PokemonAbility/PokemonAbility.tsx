4ef2687ccf7726b93e0bd6783fb893aa62e0069e
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-11-01 06:51:56 +0000

    named to .tsx

--- next-app/src/app/components/PokemonAbility/PokemonAbility.tsx
+++ next-app/src/app/components/PokemonAbility/PokemonAbility.tsx
@@ -1,4 +1,4 @@
-const PokemonAbility = ({ abilityName }) => (
+const PokemonAbility = ({ abilityName }: { abilityName: string }) => (
   <div className="pokemon-ability">
     <div className="pokemon-ability-type">Hidden ability</div>
     <div className="pokemon-ability-name">{abilityName}</div>
