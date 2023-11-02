bcbba433e033d39cf3c855c76d2c35cd2fee48d7
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-11-02 10:52:58 +0000

    tested getNormalAbility

--- src/app/components/PokemonAbility/PokemonAbility.tsx
+++ src/app/components/PokemonAbility/PokemonAbility.tsx
@@ -1,8 +1,10 @@
-const PokemonAbility = ({ abilityName }: { abilityName: string }) => (
-  <div className="pokemon-ability">
-    <div className="pokemon-ability-type">Hidden ability</div>
-    <div className="pokemon-ability-name">{abilityName}</div>
-  </div>
-);
+const PokemonAbility = ({ abilityName }: { abilityName: string }) => {
+  return (
+    <div className="pokemon-ability">
+      <div className="pokemon-ability-type">Hidden ability</div>
+      <div className="pokemon-ability-name">{abilityName}</div>
+    </div>
+  );
+};
 
 export default PokemonAbility;
---
+++ src/app/components/PokemonPage/PokemonPage.test.tsx
@@ -0,0 +1,25 @@
+import { vi } from "vitest";
+import * as helpers from "../../../helpers";
+import { Ability, Pokemon } from "@/types";
+
+describe("PokemonPage", () => {
+  test("getNormalAbility returns the normal ability", () => {
+    const mockPokemon = {
+      abilities: [
+        {
+          ability: {
+            name: "overgrow",
+            url: "https://pokeapi.co/api/v2/ability/65/",
+          },
+          is_hidden: false,
+          slot: 1,
+        },
+      ],
+    } as Pokemon;
+
+    const ability = helpers.getNormalAbility(mockPokemon) as Ability;
+
+    expect(ability.ability.name).toBe("overgrow");
+    expect(ability.is_hidden).toBeFalsy();
+  });
+});
