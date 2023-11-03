e94e2f80ec494bfcfa767211a3d4da4ea3f56bcc
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-11-03 07:09:51 +0000

    added alphabetize rule to linter

--- src/app/components/PokemonList/PokemonList.tsx
+++ src/app/components/PokemonList/PokemonList.tsx
@@ -1,6 +1,6 @@
+import Image from "next/image";
 import Link from "next/link";
 import { MappedPokemon } from "@/types";
-import Image from "next/image";
 
 const PokemonList = ({ pokemonList }: { pokemonList: MappedPokemon[] }) => {
   return (
